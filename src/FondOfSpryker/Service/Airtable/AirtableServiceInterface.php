c89deeb4feb63d6de0aa1e13eb8a7a41d0193f8a
Chris <christopher.egger@fondof.de>
2020-05-06 11:04:38 +0200

    Add specification to service interface

--- src/FondOfSpryker/Service/Airtable/AirtableServiceInterface.php
+++ src/FondOfSpryker/Service/Airtable/AirtableServiceInterface.php
@@ -8,6 +8,12 @@ use Generated\Shared\Transfer\AirtableResponseDataTransfer;
 interface AirtableServiceInterface
 {
     /**
+     * Specification:
+     * - Write data into given airtable identified by base id and table id
+     * - Returns airtable response transfer
+     *
+     * @api
+     *
      * @param string $baseId
      * @param string $tableId
      * @param \Generated\Shared\Transfer\AirtableRequestDataTransfer $airtableRequestDataTransfer
@@ -17,6 +23,12 @@ interface AirtableServiceInterface
     public function writeRecord(string $baseId, string $tableId, AirtableRequestDataTransfer $airtableRequestDataTransfer): AirtableResponseDataTransfer;
 
     /**
+     * Specification:
+     * - Return a record by id from an airtable identified by base id and table id
+     * - Returns airtable response transfer
+     *
+     * @api
+     *
      * @param string $baseId
      * @param string $tableId
      * @param string $recordId
@@ -26,6 +38,13 @@ interface AirtableServiceInterface
     public function getRecord(string $baseId, string $tableId, string $recordId): AirtableResponseDataTransfer;
 
     /**
+     * Specification:
+     * - Return all records from an airtable identified by base id and table id
+     * - Optional set the limit of records
+     * - Returns airtable response transfer
+     *
+     * @api
+     *
      * @param string $baseId
      * @param string $tableId
      * @param int|null $limit
